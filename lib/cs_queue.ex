7f3c9d669157fc7ed48233375e5e54428908b9f2
a.kurdyukov <a.kurdyukov@cti.ru>
2018-12-01 15:55:46 +0300

    #master initial commit

---
+++ lib/cs_queue.ex
@@ -0,0 +1,18 @@
+defmodule CsQueue do
+  @moduledoc """
+  Documentation for CsQueue.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CsQueue.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cs_queue_test.exs
@@ -0,0 +1,8 @@
+defmodule CsQueueTest do
+  use ExUnit.Case
+  doctest CsQueue
+
+  test "greets the world" do
+    assert CsQueue.hello() == :world
+  end
+end
