fca0f697110a9f2aeff322672579d006b8897f75
a.kurdyukov <a.kurdyukov@cti.ru>
2018-12-02 16:20:51 +0300

    #master metadata has been added

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule CsQueue.MixProject do
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       mod: [],
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -29,6 +31,21 @@ defmodule CsQueue.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description() do
+    "Simple queue client. Use mnesia for storing messages"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "cs_queue",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/CarefreeSlacker/cs_queue"}
     ]
   end
 end
